40dbf183c8186fe2946dd5a7ee9392680f0b633c
Kamyar Mivehchi <kammivehchi@gmail.com>
2022-02-08 23:14:47 -0800

    app setup

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import Display from './components/display'
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <Display />
+    </div>
+  )
+}
+
+export default App;
---
+++ portfolio/src/components/display.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './navbar';
+
+// const profile = {
+//     name: 'Kamyar Mivehchi',
+//     github: "https://github.com/Kam-Mivehchi",
+//     linkedin: "https://www.linkedin.com/in/kamyar-mivehchi/",
+// }
+
+export default function Display() {
+    return (
+
+        <Navbar />
+
+    )
+}
