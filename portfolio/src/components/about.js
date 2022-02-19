9e4df5c8981f0c62e77af6a1480b3206628f7aa2
Kamyar Mivehchi <kammivehchi@gmail.com>
2022-02-19 02:00:26 -0800

    fixed about spacing

--- portfolio/src/components/about.js
+++ portfolio/src/components/about.js
@@ -17,17 +17,16 @@ function About() {
 
                 <p className={paragraphStyles}>
                     Proven research professional with experience in the development of cleaning chemicals and semiconductor growth. As an R&D Analytical Chemist for Shepard Bros Inc, I optimized several quality control processes to improve accuracy and precision of titrations substantially. Additionally, I lead efforts to resolve customers cleaning issues by developing 2 new products. In this role, I quickly adapted to the industry and was formulating new products within 6 months.
-                    <br></br>
 
-                    <br></br>
                     Orange County Native, with a passion for learning new skills and developing problem solving strategies that will optimize your business. Currently learning JavaScript, SQL, HTML, CSS and MERN through a full stack web development boot camp from UC Irvine.
-                    <br></br>
 
-                    <br></br>
+
+                    <br />
+                    <br />
                     As for my future, I hope to work as a product/software developer for a role that can utilize my Chemistry and R&D background. Additionally, I’m interested in opportunities that utilize modern technology to optimize laboratory and quality control processes.
-                    <br></br>
 
-                    <br></br>
+
+
                     Bachelor of Science in Chemistry from UC Riverside. Soon to be graduate of Full Stack Web Development Bootcamp (Mar. 2022)
                 </p>
 
