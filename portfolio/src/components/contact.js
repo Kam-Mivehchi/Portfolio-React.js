6deaa47047ac30cda8949a8ccef12e07edd89f2a
Kamyar Mivehchi <kammivehchi@gmail.com>
2022-02-23 22:57:47 -0800

    added spacing on form

--- portfolio/src/components/contact.js
+++ portfolio/src/components/contact.js
@@ -6,11 +6,11 @@ const Contact = () => {
             <h1 className={`text-light-blue text-6xl font-black `}>Contact me</h1>
             <form action="mailto:kammivehci@gmail.com" method='POST' encType='multipart/form-data' name='EmailForm' className={`text-accentd font-bold text-3xl`}>
                 Name:<br></br>
-                <input type="text" size='40' className={`rounded-lg `} name='ContactName'></input><br></br>
+                <input type="text" size='40' className={`rounded-lg mt-2 `} name='ContactName'></input><br></br><br></br>
                 Email:<br></br>
-                <input type="text" size='40' className={`rounded-lg `} name='ContactName'></input>
+                <input type="text" size='40' className={`rounded-lg  mt-2`} name='ContactName'></input>
                 <br></br><br></br>
-                Message:<br></br> <textarea className={`rounded-lg `} name='ContactCommentt' rows='6' cols='40'>
+                Message:<br></br> <textarea className={`rounded-lg  mt-2`} name='ContactCommentt' rows='6' cols='40'>
 
 
                 </textarea> <br></br>
